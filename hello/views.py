45267b542b7f8443aad56af58e74a90ef072fd66
Glenn Do <3CuongXD@gmail.com>
2022-08-04 15:30:17 -0400

    Trendbi

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-
+from hello import form
 # Create your views here.
 def index(request):
+    new_form = form()
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return render(request, "index.html", {'form': new_form}) 
 
 
 def db(request):
