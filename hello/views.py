72e464d6f6f17bab4f8ebd3eeff8b03579f6e837
Glenn Do <3CuongXD@gmail.com>
2022-08-04 16:32:37 -0400

    Trendbi

--- hello/views.py
+++ hello/views.py
@@ -5,14 +5,12 @@ from .models import Greeting, FindTrend
 from .Main import *
 # Create your views here.
 def index(request):
-    
+    plot_div = None
     if request.method == 'POST':
         # create an instance of our form, and fill it with the POST data
-        form = FindTrend(request.POST)
-
-        if form.is_valid():
-            message=form.cleaned_data['search_term']
-            plot_div = get_graph([message])
+        message=request.POST.get('Term')
+        print(message)
+        plot_div = get_graph([message])
     # if the form is not valid, we let execution continue to the return
     # statement below, and display the form again (with errors).
 
@@ -20,7 +18,7 @@ def index(request):
     # this must be a GET request, so create an empty form
         new_form = FindTrend()
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html", {'form': new_form, 'plot_div': plot_div}) 
+    return render(request, "index.html", {'plot_div': plot_div}) 
 
 
 def db(request):
